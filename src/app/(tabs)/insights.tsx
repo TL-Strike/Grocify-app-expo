3da117345c3497c07ae7ed75b545bfdf20dd9ed2
ThaiLinh-Strike <62182718+TL-Strike@users.noreply.github.com>
2026-04-07 15:35:07 +0700

    feat: integrate Sentry for error tracking and feedback; update app configuration and add SentryFeedbackButton component

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -2,6 +2,7 @@ import ClearCompletedButton from "@/components/insights/ClearCompletedButton";
 import InsightsCategorySection from "@/components/insights/InsightsCategorySection";
 import InsightsPrioritySection from "@/components/insights/InsightsPrioritySection";
 import InsightsStatsSection from "@/components/insights/InsightsStatsSection";
+import SentryFeedbackButton from "@/components/insights/SentryFeedbackButton";
 import UserProfile from "@/components/insights/UserProfile";
 import TabScreenBackground from "@/components/TabScreenBackground";
 import React from "react";
@@ -9,19 +10,23 @@ import { ScrollView } from "react-native";
 
 const InsightScreen = () => {
   return (
-    <ScrollView
-      className="flex-1 bg-background py-4"
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ padding: 20, gap: 14 }}
-      contentInsetAdjustmentBehavior="automatic"
-    >
-      <TabScreenBackground />
-      <UserProfile />
-      <InsightsStatsSection />
-      <InsightsCategorySection />
-      <InsightsPrioritySection />
-      <ClearCompletedButton />
-    </ScrollView>
+    <>
+      <ScrollView
+        className="flex-1 bg-background py-4"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 20, gap: 14 }}
+        contentInsetAdjustmentBehavior="automatic"
+      >
+        <TabScreenBackground />
+        <UserProfile />
+        <InsightsStatsSection />
+        <InsightsCategorySection />
+        <InsightsPrioritySection />
+        <ClearCompletedButton />
+      </ScrollView>
+
+      <SentryFeedbackButton />
+    </>
   );
 };
 
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -7,6 +7,7 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import * as Sentry from "@sentry/react-native";
 import { Stack } from "expo-router";
 import React from "react";
 import { useColorScheme } from "react-native";
@@ -18,7 +19,12 @@ if (!publishableKey) {
   throw new Error("Add your Clerk Publishable Key to the .env file");
 }
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+  integrations: [Sentry.feedbackIntegration()],
+});
+
+export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
@@ -31,4 +37,4 @@ export default function RootLayout() {
       </KeyboardProvider>
     </ClerkProvider>
   );
-}
+});
